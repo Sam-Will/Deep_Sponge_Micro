5361a62b6a1c06b4d07a3cfb17df36e01e57d1c5
Sam-Will <36478721+Sam-Will@users.noreply.github.com>
2023-12-05 13:44:30 +0100

    Update Qiime2_pipeline.sh
    Updated comments

--- Qiime2_pipeline.sh
+++ Qiime2_pipeline.sh
@@ -1,9 +1,14 @@
-# COMPLETE QIIME2 PIPELINE FOR BISECT 16S STUDY ###
+## QIIME2 PIPELINE FOR BISECT 16S STUDY ###
+# Sam Williams
+# 05/12/2023
 
-# I ran fastQC on the RawData from illumina
+# First step is to do quality control on the raw illumina reads
+# fastQC on the RawData from illumina
 ./fastqc -o /Users/sw17073/Desktop/16S_metagenomics/01_FastQC /Users/sw17073/Desktop/16S_metagenomics/00_Rawdata/*/*
 
-# Activate the qiime conda enviroment
+# Activate the qiime2 conda environment
+# Qiime2 is a comprehensive package for microbiome analysis
+# Download here: https://docs.qiime2.org/2023.9/install/
 conda activate qiime2-2023.7
 
 # Next import the sequence data into Qiime2 as an qiime artifact
@@ -34,6 +39,8 @@ qiime demux summarize \
 --o-visualization 03_Qiimecutadapt/SMP_demux_trimmed-seqs.qzv
 
 # Use DADA2 on denoise the data
+# Choose your truncation lengths and trim based on quality plots
+# You should have a minimum of 30 bp overlap between reads
 qiime dada2 denoise-paired \
 --i-demultiplexed-seqs 03_Qiimecutadapt/SMP_demux_trimmed-seqs.qza \
 --p-trim-left-f 20 \
@@ -45,7 +52,7 @@ qiime dada2 denoise-paired \
 --o-representative-sequences 04_DADA2/SMP-truncr-172-truncf-269-trim20-DADA2_rep-seqs.qza \
 --o-denoising-stats 04_DADA2/SMP-truncr-172-truncf-269-trim20-DADA2_denoising-stats.qza 
 
-# Download the Greengenes2 2022.10 full length sequences classifier from here https://docs.qiime2.org/2023.7/data-resources/
+# Download the Greengenes2 2022.10 full-length sequences classifier from here https://docs.qiime2.org/2023.7/data-resources/
 # Used the full length over the 515F-805R as we have a longer sequence
 
 qiime feature-classifier classify-sklearn \
@@ -53,7 +60,8 @@ qiime feature-classifier classify-sklearn \
   --i-reads 04_DADA2/SMP-truncr-172-truncf-269-trim20-DADA2_rep-seqs.qza \
   --o-classification 05_classify/SMP-truncr-172-truncf-269-trim20-DADA2_taxonomy.qza
 
-# This is an old metadata file for the purpose of matching with Qiime2
+# You can view a quick taxa bar blot with Qiime2
+# You metadata file must be formatted corrected - See the docs
 qiime taxa barplot \
   --i-table 04_DADA2/SMP-truncr-172-truncf-269-trim20-DADA2_table.qza \
   --i-taxonomy 05_classify/SMP-truncr-172-truncf-269-trim20-DADA2_taxonomy.qza \
@@ -68,9 +76,8 @@ qiime phylogeny align-to-tree-mafft-fasttree \
   --o-tree 07_phylo/unrooted-tree.qza \
   --o-rooted-tree 07_phylo/rooted-tree.qza
 
-# Export Qiime data ready for analysis in phyloseq
+# Export Qiime data ready for further analysis in phyloseq
 qiime tools export --input-path SMP-truncr-172-truncf-279-trim10-DADA2_table.qza --output-path Exported_Files
 qiime tools export --input-path SMP-truncr-172-truncf-279-trim10-DADA2_table.qza --output-path Exported_Files
 qiime tools export --input-path SMP-truncr-172-truncf-279-trim10-DADA2_table.qza --output-path Exported_Files
 qiime tools export --input-path SMP-truncr-172-truncf-279-trim10-DADA2_table.qza --output-path Exported_Files
-
